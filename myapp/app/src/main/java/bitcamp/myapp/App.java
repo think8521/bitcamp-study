5c9867c98129b1bf1e36562c33614fcb254d6b83
think8521 <think8521@naver.com>
2023-05-30 10:53:57 +0900

    .

---
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -0,0 +1,28 @@
+package bitcamp.myapp;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("나의 목록 관리 시스템");
+    System.out.println("-------------------------------");
+
+    int no = 100;
+    String name = "홍길동";
+    int age = 20;
+    boolean working = true;
+    char gender = 'M';
+    float leftEye = 1.5f;
+    float rightEye = 1.0f;
+
+    System.out.printf("번호: %d\n", no);
+    System.out.printf("이름: %s\n", name);
+
+    System.out.printf("나이: %d\n", age);
+    
+    System.out.printf("재직자: %b\n", working);  //\n은 줄바꿈, escape character
+
+    System.out.printf("성별남자(M), 여자(W) : %c\n", gender);
+
+    System.out.printf("좌우시력: %.1f, %.1f\n", leftEye, rightEye);
+  }
+}
