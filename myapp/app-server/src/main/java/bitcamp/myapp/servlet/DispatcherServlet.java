5597295210f63f47713b30081b2f3965816bc2bd
think8521 <think8521@naver.com>
2023-09-05 11:52:12 +0900

    myapp-76

--- myapp/app-server/src/main/java/bitcamp/myapp/servlet/DispatcherServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/servlet/DispatcherServlet.java
@@ -2,7 +2,7 @@ package bitcamp.myapp.servlet;
 
 import bitcamp.myapp.config.AppConfig;
 import bitcamp.myapp.config.NcpConfig;
-import bitcamp.myapp.controller.PageController;
+import bitcamp.myapp.controller.RequestMapping;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import javax.servlet.ServletException;
@@ -12,6 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(
         value = "/app/*",
@@ -21,6 +24,7 @@ public class DispatcherServlet extends HttpServlet {
   private static final long SerialVersionUID = 1L;
 
   AnnotationConfigApplicationContext iocContainer;
+  Map<String, RequestHandlerMapping> handlerMap = new HashMap<>();
 
   @Override
   public void init() throws ServletException {
@@ -29,9 +33,25 @@ public class DispatcherServlet extends HttpServlet {
 
     String[] names = iocContainer.getBeanDefinitionNames();
     for (String name : names) {
-      System.out.printf("=> %s\n", iocContainer.getBean(name).getClass().getName());
+      registerRequestHandler(iocContainer.getBean(name));
     }
 
+  }
+
+  private void registerRequestHandler(Object bean) {
+    System.out.printf("=> %s\n", bean.getClass().getName());
+
+    Method[] methods = bean.getClass().getDeclaredMethods();
+    for (Method m : methods) {
+      RequestMapping requestMapping = m.getAnnotation(RequestMapping.class);
+      if (requestMapping == null) {
+        continue;
+      }
+
+      // request handler 메서드를 맵에 등록한다.
+      handlerMap.put(requestMapping.value(), new RequestHandlerMapping(bean, m));
+      System.out.printf("    %s - %s\n", requestMapping.value(), m.getName());
+    }
   }
 
   private static final long serialVersionUID = 1L;
@@ -43,12 +63,18 @@ public class DispatcherServlet extends HttpServlet {
 
     response.setContentType("text/html;charset=UTF-8");
 
-    // 클라이언트가 요청한 페이지 컨트롤러를 찾는다.
-    PageController pageController = (PageController) iocContainer.getBean(pageControllerPath);
+    // 클라이언트가 요청한 url의 요청 핸들러를 찾는다.
+    RequestHandlerMapping requstHasndlerMapping = handlerMap.get(pageControllerPath);
 
-    // 페이지 컨트롤러를 실행한다.
+    // request handler(요청이 들어왔을 때 호출될 메서드)를 찾는다.
+    RequestHandlerMapping requestHandlerMapping = handlerMap.get(pageControllerPath);
+    if (requestHandlerMapping == null) {
+      throw new ServletException("요청을 처리할 핸들러가 없습니다!");
+    }
+
+    // request handler 호출하기
     try {
-      String viewUrl = pageController.execute(request, response);
+      String viewUrl = (String) requestHandlerMapping.handler.invoke(requestHandlerMapping.controller, request, response);
 
       if (viewUrl.startsWith("redirect:")) {
         response.sendRedirect(viewUrl.substring(9)); // 예) redirect:/app/board/list
@@ -60,6 +86,20 @@ public class DispatcherServlet extends HttpServlet {
       throw new ServletException("요청 처리 중 오류 발생!", e);
     }
 
+
+  }
+
+  static class RequestHandlerMapping {
+    Object controller;
+    Method handler;
+
+    public RequestHandlerMapping() {
+    }
+
+    public RequestHandlerMapping(Object controller, Method handler) {
+      this.controller = controller;
+      this.handler = handler;
+    }
 
   }
 }
