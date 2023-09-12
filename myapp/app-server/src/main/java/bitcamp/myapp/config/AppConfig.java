8491e6f7756d03f3257ff31cdd04aa53c1cbf4c9
think8521 <think8521@naver.com>
2023-09-12 15:52:14 +0900

    myapp-80

--- myapp/app-server/src/main/java/bitcamp/myapp/config/AppConfig.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/config/AppConfig.java
@@ -6,8 +6,13 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.util.UrlPathHelper;
 
 // Application을 실행하는데 필요한 객체를 설정하는 일을 한다.
 
@@ -18,7 +23,9 @@ import org.springframework.web.servlet.view.JstlView;
                 pattern = "bitcamp.myapp.controller.MemberController"
         )
 )
-public class AppConfig {
+
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
 
   public AppConfig() {
     System.out.println("AppConfig() 호출됨!");
@@ -38,6 +45,27 @@ public class AppConfig {
     return vr;
   }
 
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configurer) {
+    System.out.println("AppConfig.PathMatchConfigurer() 호출됨!");
+    UrlPathHelper pathHelper = new UrlPathHelper();
+
+    // @MetrixVariable 기능 활성화
+    pathHelper.setRemoveSemicolonContent(false);
+
+    // DispatcherServlet의 MVC Path 관련 설정
+    configurer.setUrlPathHelper(pathHelper);
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    System.out.println("AppConfig.addInterceptors() 호출됨!");
+
+//    registry
+//            .addInterceptor(new MyInterceptor())
+//            .addPathPatterns("/c04_1/**");
+
+  }
 }
 
 
